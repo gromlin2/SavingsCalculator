1a4d7c0132e350da6dc1555c248c3530ceccd970
David Grömling <david@keebo.ai>
2022-10-29 11:50:53 +0200

    Apply black formatting

--- interest_calculator.py
+++ interest_calculator.py
@@ -7,8 +7,14 @@ import math
 from result_generators.cli_result_generator import CliResultGenerator
 from result_generators.html_result_generator import HtmlResultGenerator
 
-def calculate_rows(initial_net_worth: float, monthly_saving: float, interest_rate: float,
-                   months: int, interest_payment: str):
+
+def calculate_rows(
+    initial_net_worth: float,
+    monthly_saving: float,
+    interest_rate: float,
+    months: int,
+    interest_payment: str,
+):
     """
     Creates a generator for all payment information per month
     :param initial_net_worth: Initial investment or total loan amount
@@ -18,9 +24,9 @@ def calculate_rows(initial_net_worth: float, monthly_saving: float, interest_rat
     :param interest_payment: The way how interest is calculated.
     :return: A generator for each month.
     """
-    if interest_payment == 'continuous':
+    if interest_payment == "continuous":
         monthly_interest = math.pow(1 + interest_rate / 100, 1.0 / 12.0) - 1
-    elif interest_payment == 'monthly':
+    elif interest_payment == "monthly":
         monthly_interest = interest_rate / (12 * 100)
     else:
         raise NotImplementedError
@@ -35,38 +41,76 @@ def calculate_rows(initial_net_worth: float, monthly_saving: float, interest_rat
         current_net_worth = current_net_worth + interest + monthly_saving
 
         total_interest = total_interest + interest
-        interest_ratio = 'NaN' if current_net_worth == 0 else total_interest / current_net_worth
+        interest_ratio = (
+            "NaN" if current_net_worth == 0 else total_interest / current_net_worth
+        )
 
-        yield {'month': month + 1, 'year': int(month / 12) + 1, 'invested': total_payments,
-               'net_worth': current_net_worth, 'interest': interest,
-               'total_interest': total_interest,
-               'interest_ratio': interest_ratio}
+        yield {
+            "month": month + 1,
+            "year": int(month / 12) + 1,
+            "invested": total_payments,
+            "net_worth": current_net_worth,
+            "interest": interest,
+            "total_interest": total_interest,
+            "interest_ratio": interest_ratio,
+        }
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate interests and savings.")
-    parser.add_argument('--initial_amount', type=float, default=0, help='Initial net worth.')
-    parser.add_argument('--monthly_savings', type=float, default=0,
-                        help='The amount that\'s being put aside every month.')
-    parser.add_argument('--interest_rate', type=float, default=0,
-                        help='The annual interest rate you expect in percent.')
-    parser.add_argument('--months', type=int, default=12,
-                        help='Number of months the interest rate is calculated for')
-    parser.add_argument('--interest_payment', type=str, choices=['continuous', 'monthly'],
-                        default='continuous',
-                        help='Weather interest is paid continuously or once a month')
-    parser.add_argument('--output_format', type=str, choices=['ascii_table', 'html'],
-                        default='ascii_table',
-                        help='Weather the output is printed as an ASCII table or HTML')
-    parser.add_argument('--output', type=str, help='File, to which the output is written.'
-                                                   'If not provided, output is written to CLI')
+    parser.add_argument(
+        "--initial_amount", type=float, default=0, help="Initial net worth."
+    )
+    parser.add_argument(
+        "--monthly_savings",
+        type=float,
+        default=0,
+        help="The amount that's being put aside every month.",
+    )
+    parser.add_argument(
+        "--interest_rate",
+        type=float,
+        default=0,
+        help="The annual interest rate you expect in percent.",
+    )
+    parser.add_argument(
+        "--months",
+        type=int,
+        default=12,
+        help="Number of months the interest rate is calculated for",
+    )
+    parser.add_argument(
+        "--interest_payment",
+        type=str,
+        choices=["continuous", "monthly"],
+        default="continuous",
+        help="Weather interest is paid continuously or once a month",
+    )
+    parser.add_argument(
+        "--output_format",
+        type=str,
+        choices=["ascii_table", "html"],
+        default="ascii_table",
+        help="Weather the output is printed as an ASCII table or HTML",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        help="File, to which the output is written."
+        "If not provided, output is written to CLI",
+    )
     args = parser.parse_args()
 
-    result = calculate_rows(args.initial_amount, args.monthly_savings, args.interest_rate,
-                            args.months, args.interest_payment)
-    if args.output_format == 'ascii_table':
+    result = calculate_rows(
+        args.initial_amount,
+        args.monthly_savings,
+        args.interest_rate,
+        args.months,
+        args.interest_payment,
+    )
+    if args.output_format == "ascii_table":
         RESULT_STRING = CliResultGenerator.generate(result)
-    elif args.output_format == 'html':
+    elif args.output_format == "html":
         RESULT_STRING = HtmlResultGenerator.generate(result)
     else:
         raise NotImplementedError
